f55405e293b5915b16bb5fb2953bff1ea402fef9
balaji-githubstore <31404824+balaji-githubstore@users.noreply.github.com>
2022-05-30 11:17:18 +0530

    slash

--- src/com/brillio/print/HelloWorld.java
+++ src/com/brillio/print/HelloWorld.java
@@ -5,7 +5,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		
 		System.out.println("Hello world \\ 123456656");
-		System.out.println("Balaji Dinakaran!");
+		System.out.println("Balaji \"Dina\"  karan!");
 		
 		System.out.print("Hello");
 		
